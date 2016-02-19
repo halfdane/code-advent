50271e26ddd8a376c55ea89520e6e855fb149187
Halfdane <halfdane@gmx.net>
2016-02-19 12:49:49 +0100

    day03 part1

--- src/code_advent/day03.clj
+++ src/code_advent/day03.clj
@@ -2,25 +2,47 @@
   (:gen-class)
   (:use clojure.test))
 
-
-
-(defn move [dir pos]
+(defn move [dir]
+  (println dir)
   (case dir
-    ">" (map + [+1 0] pos) 
-    "<" (map + [-1 0] pos)
-    "^" (map + [0 +1] pos) 
-    "v" (map + [0 -1] pos)))
+    \> '(+1 0)
+    \< '(-1 0)
+    \^ '(0 +1) 
+    \v '(0 -1)
+    '(0 0)))
 
-(defn deliver [directions]
-  2)
+(defn moves [dirs]
+  (map move dirs))
+
+(defn positions-of [dirs]
+  (reductions (partial map +) (moves dirs)))
+
+(defn positions-from-home-of [dirs]
+  (conj (positions-of dirs) '(0 0)))
+
+(defn unique-positions [dirs]
+  (seq (set (positions-from-home-of dirs))))
+
+(defn count-positions [dirs]
+  (count (unique-positions dirs)))
 
 (deftest examples
-  (is (== 2 (deliver ">")))
-  (is (== 4 (deliver "^>v<")))
-  (is (== 2 (deliver "^v^v^v^v^v"))))
+  (is (== 2 (count-positions ">")))
+  (is (== 4 (count-positions "^>v<")))
+  (is (== 2 (count-positions "^v^v^v^v^v"))))
 
-(deftest moves
-  (is (= [4 8] (move ">" [3 8])))
-  (is (= [2 8] (move "<" [3 8])))
-  (is (= [3 9] (move "^" [3 8])))
-  (is (= [3 7] (move "v" [3 8]))))
+(deftest should-collect-moves
+  (is (= '((0 1) (0 1) (1 0) (0 -1)) (moves "^^>v"))))
+
+(deftest should-add-up-moves
+  (is (= '((0 1) (0 2) (1 2) (1 1)) (positions-of "^^>v"))))
+
+(deftest should-include-home
+  (is (= '((0 0) (0 1) (0 2) (1 2) (1 1)) 
+         (positions-from-home-of "^^>v"))))
+
+(deftest should-have-unique-positions
+  (is (= '((0 0) (1 0)) (unique-positions "><><><><"))))
+
+(defn solve []
+  (println (count-positions (slurp "resources/day03.txt"))))
