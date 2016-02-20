a774770db894ece347ad3f8df2cdcc3e3a79d454
Halfdane <halfdane@gmx.net>
2016-02-20 08:54:38 +0100

    beginning day05.2

--- src/code_advent/day05.clj
+++ src/code_advent/day05.clj
@@ -25,17 +25,6 @@
    (substring? "pq" input)
    (substring? "xy" input)))
 
-(defn typeof [input]
-  (if (and 
-       (vowels? 3 input)
-       (repeats? input)
-       (not (forbidden-strings? input)))
-    :nice
-    :naughty))
-
-(defn nice? [input]
-  (= :nice (typeof input)))
-
 (deftest should-find-three-vowels
   (is (not (= true (vowels? 3 "agi"))))
   (is (= true (vowels? 3 "aei")))
@@ -47,14 +36,47 @@
   (is (= true (repeats? "abcdde")))
   (is (= true (repeats? "aabbccdd"))))
 
-(deftest should-find-naughties
-  (is (= :naughty (typeof "jchzalrnumimnmhp")))
-  (is (= :naughty (typeof "haegwjzuvuyypxyu")))
-  (is (= :naughty (typeof "dvszwmarrgswjxmb"))))
+(defn first-rules [input]
+  (if (and 
+       (vowels? 3 input)
+       (repeats? input)
+       (not (forbidden-strings? input)))
+    :nice
+    :naughty))
 
-(deftest should-find-nicies
-  (is (= :nice (typeof "ugknbfddgicrmopn")))
-  (is (= :nice (typeof "aaa"))))
+(deftest should-find-naughties-by-first-rules
+  (is (= :naughty (first-rules "jchzalrnumimnmhp")))
+  (is (= :naughty (first-rules "haegwjzuvuyypxyu")))
+  (is (= :naughty (first-rules "dvszwmarrgswjxmb"))))
+
+(deftest should-find-nicies-by-first-rules
+  (is (= :nice (first-rules "ugknbfddgicrmopn")))
+  (is (= :nice (first-rules "aaa"))))
+
+(defn char-pairs [input]
+  (partition 2 1 input))
+
+(deftest should-get-char-pairs
+  (is (= '((\x \y) (\y \x) (\x \y)) 
+         (char-pairs "xyxy")))
+  (is (= '((\a \a) (\a \b) (\b \c) (\c \d) (\d \e) (\e \f) (\f \g) (\g \a) (\a \a)) 
+         (char-pairs "aabcdefgaa"))))
+
+(deftest should-filter-same-chars-with-others-between)
+
+(defn second-rules [input]
+  (if 
+      (vowels? 3 input)
+    :nice
+    :naughty))
+
+(defn nice? [rule input]
+  (= :nice (rule input)))
 
 (defn solve []
-  (count (filter nice? (split-lines (slurp "resources/day05.txt")))))
+  (let [filter-1 (partial nice? first-rules)
+        filter-2 (partial nice? second-rules)
+        data (split-lines (slurp "resources/day05.txt"))]
+    [(count (filter filter-1 data))
+     (count (filter filter-2 data))]))
+
